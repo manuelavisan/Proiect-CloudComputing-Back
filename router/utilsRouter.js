5a59f6f3cd3dd98a27a7db5591ab6df453bde809
Visan Andreea Manuela <visanandreea18@stud.ase.ro>
2022-05-12 07:59:00 +0300

    I've added image recognition functionality

--- router/utilsRouter.js
+++ router/utilsRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {detectLanguage, translateText} = require("../utils/translateFunctions");
 const {sendMail} = require("../utils/mailFunctions");
 const {LANGUAGE_ISO_CODE} = require("../utils/dictionaries");
+const {detectLabels, detectImageProperties} = require("../utils/imageRecognitionFunctions");
 
 router.get("/detect", async (req, res) =>{
     const {text} = req.body;
@@ -45,6 +46,21 @@ router.post("/send", (req, res) => {
 
     sendMail(receiverMail, senderMail, messageContent, `${senderName} has sent you a message. Read it.`);
     res.send(200);
+})
+
+router.get("/labels", async (req, res) => {
+    const {link} = req.body;
+    if(!link){
+        return res.status(400).send("Missing parameters!");
+    }
+    const labels = await detectLabels(link);
+
+    const dominantColors = await detectImageProperties(link);
+
+    return res.json({
+        labels,
+        dominantColors
+    })
 })
 
 module.exports = router;
